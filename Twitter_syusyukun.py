090753ad302cdc4906cb278fbfa191914054d572
Umisyo <kusunokisouta@gmail.com>
2019-01-19 05:50:34 +0900

    追記モード、読み込み終わってから書き込み

--- Twitter_syusyukun.py
+++ Twitter_syusyukun.py
@@ -17,12 +17,12 @@ def main():
             tweet_data.append([tweet.id,tweet.created_at,tweet.text.replace('\n',''),tweet.favorite_count,tweet.retweet_count])
             df = pd.DataFrame(tweet_data)
             print(df)
-            df.to_csv('\\Users\\user\\awesome\\my_ai\\tweets.csv')
             count += 1
 
             if count == 100:
                 break
         
+        df.to_csv('\\Users\\user\\awesome\\my_ai\\tweets.csv', mode='a', header='false')
         time.sleep(3600)
 
 if __name__ == "__main__":
