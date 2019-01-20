9103f73a7952b988536eab8ec287412ff492b2d0
Umisyo <kusunokisouta@gmail.com>
2019-01-21 00:19:23 +0900

    3個で連鎖するようにした

--- bunnsyo_seiseikunn.py
+++ bunnsyo_seiseikunn.py
@@ -20,33 +20,34 @@ def main():
     markov = {}
     w1 = ''
     w2 = ''
+    w3 = ''
     for word in wordlist:
-        if w1 and w2:
-            if (w1, w2) not in markov:
-                markov[(w1, w2)] = []
+        if w1 and w2 and w3:
+            if (w1, w2, w3) not in markov:
+                markov[(w1, w2, w3)] = []
                 #print('w1 not in markov ', w1)
                 #print('w2 not in markov ', w2)
-            markov[(w1, w2)].append(word)
+            markov[(w1, w2, w3)].append(word)
             #print('w1 append:', w1)
             #print('w2 append:', w2)
-        w1, w2 = w2, word
+        w1, w2, w3 = w2, w3, word
         #print('w1:', w1)
         #print('w2:', w2)
     # 文章の自動作成
     count = 0
     sentence = ""
-    w1, w2 = random.choice(list(markov.keys()))
+    w1, w2, w3 = random.choice(list(markov.keys()))
 
     N = range(100)
     while count < random.choice(N):
-        tmp = random.choice(markov[(w1, w2)])
+        tmp = random.choice(markov[(w1, w2, w3)])
         sentence += tmp
-        w1, w2 = w2, tmp
+        w1, w2, w3 = w2, w3, tmp
         count += 1
     
     print(sentence.strip())
         
-    api.update_status(sentence.strip())
+    #api.update_status(sentence.strip())
 while True:
     main()
-    time.sleep(300)
+    #time.sleep(300)
